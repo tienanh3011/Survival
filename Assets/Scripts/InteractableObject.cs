c40a77f82b45c1eb33bdda1ddc53d8b0c2ec7dbf
DESKTOP-CR4RIQ2\WIN10 <comdom0123456@gmail.com>
2022-12-28 20:48:22 +0700

    Gan cay + tao AI

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string ItemName;
+    public string GetItemName()
+    {
+        return ItemName;
+    }
+}
