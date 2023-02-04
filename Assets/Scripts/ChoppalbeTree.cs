5ad805cefd27dfe1304386c8d6a130813e35dd52
DESKTOP-CR4RIQ2\WIN10 <comdom0123456@gmail.com>
2023-02-04 08:34:00 +0700

    fix linh tinh

--- Assets/Scripts/ChoppalbeTree.cs
+++ Assets/Scripts/ChoppalbeTree.cs
@@ -48,7 +48,7 @@ public class ChoppalbeTree : MonoBehaviour
         canBeChopped = false;
         SelectionManager.Instance.selectedTree = null;
         SelectionManager.Instance.chopHolder.gameObject.SetActive(false);
-        GameObject brokenTree = Instantiate(Resources.Load<GameObject>("ChoppedTree"), new Vector3(treePosition.x,treePosition.y+1.2f,treePosition.z), Quaternion.Euler(0, 0, 0));
+        GameObject brokenTree = Instantiate(Resources.Load<GameObject>("ChoppedTree"), new Vector3(treePosition.x,treePosition.y+1.8f,treePosition.z), Quaternion.Euler(0, 0, 0));
     }
     private void Update()
     {
